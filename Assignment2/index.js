56dbcb63d660b270d3e9bdd6c12bec9fc8b31b38
KushwahaVikashJ <kushwahavicky743@gmail.com>
2020-08-17 12:03:15 +0530

    initial commit:Assignment2

---
+++ Assignment2/index.js
@@ -0,0 +1,19 @@
+document.getElementById('index').addEventListener('click',()=>{
+    window.open('index.html');
+})
+
+document.getElementById('cakes').addEventListener('click',()=>{
+    window.open('cakes.html');
+})
+
+document.getElementById('cupCakes').addEventListener('click',()=>{
+    window.open('cupCake.html');
+})
+
+document.getElementById('sweets').addEventListener('click',()=>{
+    window.open('sweet.html');
+})
+
+document.getElementById('doughnuts').addEventListener('click',()=>{
+    window.open('doughnut.html');
+})
